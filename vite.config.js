609fca937118274af399bebc66eb27db2b0035a6
rishu <rishuchaudhary417@gmail.com>
2025-07-29 16:17:30 +0530

    add zodiac api and new UI of this

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,23 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://horoscope-app-api.vercel.app",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "/api/v1"),
+      },
+      "/api/insertContractDetails": {
+        target: "https://asto-om-backend.vercel.app",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
+      },
+      "/api/insertInquiryDetails": {
+        target: "https://asto-om-backend.vercel.app",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
+      },
+    },
+  },
 });
