d6771c63f0e0f408ef49fde9124b859d7ac5108d
Brijesh Bhatt <brijeshbhatt5699@gmail.com>
2025-12-20 11:14:28 +0530

    vite config issue

--- src/utils/lazyLoad.jsx
+++ src/utils/lazyLoad.jsx
@@ -1,4 +1,28 @@
 import React from "react";
-// test
-export const LazyLoad = (path) =>
-  React.lazy(() => import(`../components/${path}`));
+
+const componentMap = {
+  Home: () => import("../components/Home"),
+  AboutUs: () => import("../components/AboutUs"),
+  Awards: () => import("../components/Awards"),
+  Certificate: () => import("../components/Certificate"),
+  ContactUs: () => import("../components/ContactUs"),
+  FamilyProblemSolution: () => import("../components/FamilyProblemSolution"),
+  HoroscopeReading: () => import("../components/HoroscopeReading"),
+  HusbandWifeDispute: () => import("../components/HusbandWifeDispute"),
+  LoveMarriageProblem: () => import("../components/LoveMarriageProblem"),
+  LoveProblemSolution: () => import("../components/LoveProblemSolution"),
+  MarriageProblemSolution: () =>
+    import("../components/MarriageProblemSolution"),
+  PhotoGallery: () => import("../components/PhotoGallery"),
+  VideoGallery: () => import("../components/VideoGallery"),
+  PrivacyPolicy: () => import("../components/PrivacyPolicy"),
+  Inquiry: () => import("../components/Inquiry"),
+  ZodiacSigns: () => import("../components/ZodiacSigns"),
+  ZodiacDetail: () => import("../components/ZodiacDetail"),
+
+  "Admin/Login": () => import("../components/Admin/Login"),
+  "Admin/Register": () => import("../components/Admin/Register"),
+  "Admin/Dashboard": () => import("../components/Admin/Dashboard"),
+};
+
+export const LazyLoad = (name) => React.lazy(componentMap[name]);
